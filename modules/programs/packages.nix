60916e824d09ac5cc5979760d14ee31dc6248c24
racoot <sukhmanghum66778899@gmail.com>
2024-03-22 09:57:06 -0400

    updated: pass.nix

--- modules/programs/packages.nix
+++ modules/programs/packages.nix
@@ -67,7 +67,6 @@
       glib                   # C library
 #      tor-browser-bundle-bin
       projectlibre        # MS Project alternative
-      notion-app-enhanced # Notion client with notion enhancer for linux
 
       #### System hardening
 
