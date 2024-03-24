85247dbd1e6e37d681f4022b34b7874b3b1f1aa0
racoot <sukhmanghum66778899@gmail.com>
2024-03-23 21:35:36 -0400

    added: nftables

--- modules/services/samba.nix
+++ modules/services/samba.nix
@@ -4,49 +4,36 @@
 
 { config, lib, vars, ... }:
 
-with lib;
 {
-  options.samba = {
-    enable = mkOption {
-      type = types.bool;
-      default = false;
-      description = mdDoc ''
-        Samba - Network shares
+  services = {
+    samba = {
+      enable = true;
+      securityType = "user";
+      extraConfig = ''
+        workgroup = WORKGROUP
+        server string = smbnix
+        netbios name = smbnix
+        security = user 
+        #max protocol = smb2
+        # note: localhost is the ipv6 localhost ::1
+        hosts allow = 192.168.122. 127.0.0.1 localhost
+        hosts deny = 0.0.0.0/0
+        guest account = nobody
+        map to guest = bad user
+        server signing = auto
+        deadtime = 30
+        use sendfile = yes
+        min receivefile size = 16384
       '';
-    };
-  };
-
-  config = mkIf config.samba.enable {
-    services = {
-      samba = {
-        enable = true;
-        securityType = "user";
-        extraConfig = ''
-          workgroup = WORKGROUP
-          server string = smbnix
-          netbios name = smbnix
-          security = user 
-          #max protocol = smb2
-          # note: localhost is the ipv6 localhost ::1
-          hosts allow = 192.168.122. 127.0.0.1 localhost
-          hosts deny = 0.0.0.0/0
-          guest account = nobody
-          map to guest = bad user
-          server signing = auto
-          deadtime = 30
-          use sendfile = yes
-          min receivefile size = 16384
-        '';
-        shares = {                                # Set Password: $ smbpasswd -a <user>
-          share = {
-            path = "/home/${vars.user}/share";
-            browseable = "yes";
-            "guest ok" = "no";
-            "read only" = "no";
-          };
+      shares = {                                # Set Password: $ smbpasswd -a <user>
+        share = {
+          path = "/home/${vars.user}/share";
+          browseable = "yes";
+          "guest ok" = "no";
+          "read only" = "no";
         };
-        openFirewall = true;
       };
+      openFirewall = true;
     };
   };
 }
